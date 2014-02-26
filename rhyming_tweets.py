ca5d798bd7e1740f1115401940ad5a4dff404c56
dchen <dchen@ubuntu.(none)>
2014-02-26 13:20:50 -0500

    conflicting changes in rhyming_tweets

---
+++ rhyming_tweets.py
@@ -0,0 +1,12 @@
+#import modules needed to pull tweets
+#import python TLK probably to get rhyming words
+#try using topsy and otter.topsy.com
+
+def pull_tweets('keyword'):
+    """creates a textfile of tweets pulled using pattern's search engine function
+        
+    """
+    print 'placeholder'
+
+def pull
+    
