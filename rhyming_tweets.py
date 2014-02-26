8c8dba9ae0a31ab948d669585dd41e827086686d
hzhu <hzhu@hzhu.(none)>
2014-02-26 13:20:12 -0500

    I'm commiting.

---
+++ rhyming_tweets.py
@@ -0,0 +1,11 @@
+from pattern.web import Twitter
+import time
+
+# s = Twitter().stream('fail')
+# for i in range(10):
+#     time.sleep(1)
+#     s.update(bytes=1024)
+#     print s[-1].text if s else ''
+
+if __name == "__main__":
+    print hello
